1c458d9a192174004036280fec1646ee40b199ad
haikal5352 <haikaladitama35@gmail.com>
2026-01-09 12:00:47 +0700

    Add project files.

---
+++ AplikasiSampahJabar/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace AplikasiSampahJabar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKabupaten = new System.Windows.Forms.TextBox();
+            this.txtJenis = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnExportPDF = new System.Windows.Forms.Button();
+            this.btnKirimChat = new System.Windows.Forms.Button();
+            this.rtbChatArea = new System.Windows.Forms.RichTextBox();
+            this.txtChatInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(600, 400);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(618, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kabupaten/Kota";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(618, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Jenis Sampah";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(618, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Volume (Ton)";
+            // 
+            // txtKabupaten
+            // 
+            this.txtKabupaten.Location = new System.Drawing.Point(622, 35);
+            this.txtKabupaten.Name = "txtKabupaten";
+            this.txtKabupaten.Size = new System.Drawing.Size(100, 26);
+            this.txtKabupaten.TabIndex = 4;
+            // 
+            // txtJenis
+            // 
+            this.txtJenis.Location = new System.Drawing.Point(618, 137);
+            this.txtJenis.Name = "txtJenis";
+            this.txtJenis.Size = new System.Drawing.Size(100, 26);
+            this.txtJenis.TabIndex = 5;
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(618, 264);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(100, 26);
+            this.txtVolume.TabIndex = 6;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(548, 609);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 7;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(629, 609);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 8;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnExportPDF
+            // 
+            this.btnExportPDF.Location = new System.Drawing.Point(710, 609);
+            this.btnExportPDF.Name = "btnExportPDF";
+            this.btnExportPDF.Size = new System.Drawing.Size(75, 23);
+            this.btnExportPDF.TabIndex = 9;
+            this.btnExportPDF.Text = "Export ke PDF";
+            this.btnExportPDF.UseVisualStyleBackColor = true;
+            this.btnExportPDF.Click += new System.EventHandler(this.btnExportPDF_Click);
+            // 
+            // btnKirimChat
+            // 
+            this.btnKirimChat.Location = new System.Drawing.Point(791, 609);
+            this.btnKirimChat.Name = "btnKirimChat";
+            this.btnKirimChat.Size = new System.Drawing.Size(75, 23);
+            this.btnKirimChat.TabIndex = 10;
+            this.btnKirimChat.Text = "Kirim ke Chat";
+            this.btnKirimChat.UseVisualStyleBackColor = true;
+            this.btnKirimChat.Click += new System.EventHandler(this.btnKirimChat_Click);
+            // 
+            // rtbChatArea
+            // 
+            this.rtbChatArea.Location = new System.Drawing.Point(12, 418);
+            this.rtbChatArea.Name = "rtbChatArea";
+            this.rtbChatArea.Size = new System.Drawing.Size(599, 96);
+            this.rtbChatArea.TabIndex = 11;
+            this.rtbChatArea.Text = "";
+            // 
+            // txtChatInput
+            // 
+            this.txtChatInput.Location = new System.Drawing.Point(12, 520);
+            this.txtChatInput.Name = "txtChatInput";
+            this.txtChatInput.Size = new System.Drawing.Size(100, 26);
+            this.txtChatInput.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 644);
+            this.Controls.Add(this.txtChatInput);
+            this.Controls.Add(this.rtbChatArea);
+            this.Controls.Add(this.btnKirimChat);
+            this.Controls.Add(this.btnExportPDF);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtJenis);
+            this.Controls.Add(this.txtKabupaten);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKabupaten;
+        private System.Windows.Forms.TextBox txtJenis;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnExportPDF;
+        private System.Windows.Forms.Button btnKirimChat;
+        private System.Windows.Forms.RichTextBox rtbChatArea;
+        private System.Windows.Forms.TextBox txtChatInput;
+    }
+}
+
---
+++ AplikasiSampahJabar/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+// Tambahkan using lain yang diperlukan
+
+namespace AplikasiSampahJabar
+{
+    public partial class Form1 : Form
+    {
+        // 1. Variabel Global untuk MongoDB [cite: 54]
+        private MongoClient client;
+
+        private IMongoDatabase database;
+        private IMongoCollection<BsonDocument> collection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMongoDB(); // Panggil koneksi saat aplikasi mulai [cite: 231]
+            TampilDataKeGrid();  // Load data awal
+        }
+
+        // 2. Method Koneksi Database
+        private void InitializeMongoDB()
+        {
+            try
+            {
+                // Baca connection string dari file config
+                string connectionString;
+                if (System.IO.File.Exists("config.txt"))
+                {
+                    connectionString = System.IO.File.ReadAllText("config.txt").Trim();
+                }
+                else
+                {
+                    connectionString = "mongodb+srv://averouse:averouse@trashedcluster.izsn7ac.mongodb.net/?appName=TrashedCluster";
+                }
+                
+                client = new MongoClient(connectionString);
+                database = client.GetDatabase("db_sampah_jabar");
+                collection = database.GetCollection<BsonDocument>("data_sampah");
+                
+                // Test koneksi
+                database.RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                MessageBox.Show("Berhasil terhubung ke MongoDB Atlas!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Koneksi MongoDB Atlas gagal: {ex.Message}\nAplikasi akan berjalan dengan data dummy.");
+                collection = null;
+            }
+        }
+
+        private void TampilDataKeGrid()
+        {
+            if (collection == null)
+            {
+                // Jika tidak ada koneksi database, tampilkan data dummy
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Kabupaten");
+                dt.Columns.Add("JenisSampah");
+                dt.Columns.Add("Volume");
+                dt.Columns.Add("WaktuInput");
+                
+                DataRow row = dt.NewRow();
+                row["Kabupaten"] = "Bandung";
+                row["JenisSampah"] = "Organik";
+                row["Volume"] = "10";
+                row["WaktuInput"] = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                dt.Rows.Add(row);
+                
+                dataGridView1.DataSource = dt;
+                return;
+            }
+
+            // Ambil semua data dari MongoDB
+            var documents = collection.Find(new BsonDocument()).ToList();
+            DataTable dt2 = new DataTable();
+
+            if (documents.Count > 0)
+            {
+                // A. Buat Kolom Header Otomatis dari Data Pertama [cite: 73]
+                foreach (var element in documents[0].Elements)
+                {
+                    dt2.Columns.Add(element.Name); // Mengambil nama key sebagai judul kolom [cite: 82]
+                }
+
+                // B. Isi Baris Data [cite: 89]
+                foreach (var doc in documents)
+                {
+                    DataRow row = dt2.NewRow();
+                    foreach (var element in doc.Elements)
+                    {
+                        row[element.Name] = element.Value.ToString(); // Isi nilai per sel [cite: 99]
+                    }
+                    dt2.Rows.Add(row); // Masukkan baris ke tabel [cite: 105]
+                }
+            }
+
+            dataGridView1.DataSource = dt2; // Tampilkan ke layar [cite: 112]
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            if (collection == null)
+            {
+                MessageBox.Show("Tidak ada koneksi database. Data tidak dapat disimpan.");
+                return;
+            }
+
+            // Membuat dokumen BSON baru dari inputan user
+            var dokumenBaru = new BsonDocument
+    {
+        { "Kabupaten", txtKabupaten.Text },
+        { "JenisSampah", txtJenis.Text },
+        { "Volume", txtVolume.Text },
+        { "WaktuInput", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") }
+    };
+
+            collection.InsertOne(dokumenBaru); // Kirim ke MongoDB Atlas
+            MessageBox.Show("Data berhasil disimpan!");
+            TampilDataKeGrid(); // Refresh tabel
+        }
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            // Cek apakah ada baris yang dipilih
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                // Ambil ID dari baris yang dipilih (pastikan kolom _id ada di grid)
+                // Kolom 0 biasanya adalah "_id" jika kita load semua dari MongoDB
+                var idSampah = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                var objectId = ObjectId.Parse(idSampah);
+
+                // Hapus dari Database
+                var filter = Builders<BsonDocument>.Filter.Eq("_id", objectId);
+                collection.DeleteOne(filter);
+
+                MessageBox.Show("Data berhasil dihapus!");
+                TampilDataKeGrid(); // Refresh tabel
+            }
+            else
+            {
+                MessageBox.Show("Pilih baris data yang mau dihapus dulu!");
+            }
+        }
+
+        private void btnExportPDF_Click(object sender, EventArgs e)
+        {
+            // 1. Siapkan dialog penyimpanan file [cite: 147]
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PDF Files|*.pdf";
+            sfd.Title = "Simpan Laporan Sampah";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                // 2. Ambil data terbaru [cite: 145]
+                var documents = collection.Find(new BsonDocument()).ToList();
+
+                // 3. Proses penulisan PDF menggunakan iText7 [cite: 156]
+                using (PdfWriter writer = new PdfWriter(sfd.FileName))
+                {
+                    using (PdfDocument pdf = new PdfDocument(writer))
+                    {
+                        Document doc = new Document(pdf); // Membuat halaman dokumen [cite: 168]
+
+                        // Judul Laporan
+                        doc.Add(new Paragraph("Laporan Data Sampah Jawa Barat").SetFontSize(18));
+
+                        // 4. Loop data dan masukkan ke PDF [cite: 171]
+                        foreach (var d in documents)
+                        {
+                            // Mengambil isi dokumen dan menjadikannya paragraf [cite: 175]
+                            string barisData = $"Wilayah: {d["Kabupaten"]}, Jenis: {d["JenisSampah"]}, Volume: {d["Volume"]} Ton";
+                            doc.Add(new Paragraph(barisData));
+                        }
+                    }
+                }
+                MessageBox.Show("Laporan PDF berhasil dibuat!");
+            }
+        }
+
+
+        private void btnKirimChat_Click(object sender, EventArgs e)
+        {
+            string pertanyaan = txtChatInput.Text.ToLower();
+            string jawaban = "";
+
+            // Logika chatbot sederhana
+            if (pertanyaan.Contains("halo") || pertanyaan.Contains("selamat"))
+            {
+                jawaban = "Halo! Saya asisten data sampah Jabar. Ada yang bisa dibantu?";
+            }
+            else if (pertanyaan.Contains("total") || pertanyaan.Contains("jumlah"))
+            {
+                // Fitur pintar: Menghitung jumlah data di database
+                long jumlahData = collection.CountDocuments(new BsonDocument());
+                jawaban = $"Saat ini tercatat ada {jumlahData} entri data sampah di sistem.";
+            }
+            else if (pertanyaan.Contains("siapa yang buat"))
+            {
+                jawaban = "Aplikasi ini dibuat oleh Mahasiswa ULBI yang rajin.";
+            }
+            else
+            {
+                jawaban = "Maaf, saya belum mengerti. Coba tanya tentang 'jumlah data'.";
+            }
+
+            // Tampilkan chat
+            rtbChatArea.AppendText("User: " + txtChatInput.Text + "\n");
+            rtbChatArea.AppendText("Bot: " + jawaban + "\n\n");
+            txtChatInput.Clear();
+        }
+    }
+}
